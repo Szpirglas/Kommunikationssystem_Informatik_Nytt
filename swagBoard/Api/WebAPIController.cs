9bf035cad6d2d0d9acbac3aca8923fcf1a3156de
anton lindberg <ant-on26@hotmail.com>
2016-04-18 14:07:24 +0200

    hejhej

--- swagBoard/Api/WebAPIController.cs
+++ swagBoard/Api/WebAPIController.cs
@@ -20,11 +20,14 @@ namespace Whiteboard.API
         BlogEntryRepository blogRep;
         Category_BlogRepository cb_rep;
         FileRepository fileRep;
+        UserRepository userRep;
+
         public WebAPIController()
         {
             blogRep = new BlogEntryRepository();
             cb_rep = new Category_BlogRepository();
             fileRep = new FileRepository();
+            userRep = new UserRepository();
         }
 
 
@@ -33,15 +36,19 @@ namespace Whiteboard.API
 
         [System.Web.Mvc.HttpPost]
         [ValidateAntiForgeryToken]
-        public void sendPost(string title, int section, int sender, string content, string categoryIds)
+        public void sendPost(string title, int section, string content, string categoryIds)
         {
+
+            var sender=userRep.getYaUserFromYaMail(User.Identity.Name);
+
+             
             if (!string.IsNullOrWhiteSpace(content) || !string.IsNullOrWhiteSpace(title))
             {
                 var blogToPost = new BlogEntryModel
                 {
                     Title = title,
                     Section = section,
-                    Sender = sender,
+                    Sender = sender.Id,
                     Content = content,
                     Date = DateTime.Now
                 };
